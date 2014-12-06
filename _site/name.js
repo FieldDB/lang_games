23d9e5406d5256aecbb25b283ceb01e3ff992c26
Carolyn Anderson <canders277@gmail.com>
2014-12-06 15:15:21 -0800

    trying to fix js

--- _site/name.js
+++ _site/name.js
@@ -7,5 +7,5 @@ var el = document.getElementsByClassName("name");
 for (var i = 0; i < el.length; i++){
     console.log(el);
     console.log(el[i]);
-    el[i].addEventListener("click", function(){changeColor(el[i])}, false);
+    el[i].addEventListener("click", changeColor(el[i]), false);
 }
