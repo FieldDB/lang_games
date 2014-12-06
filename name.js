b1a6df007ed1a18d5d1f2030580845248cf86ed9
Carolyn Anderson <canders277@gmail.com>
2014-12-06 15:11:26 -0800

    testing

--- name.js
+++ name.js
@@ -1,9 +1,9 @@
 function changeColor(item) {
     console.log(item);
-  item.style = "color: red";    
+  item.style = "color:red";    
 }
 
-var el = document.getElementsByTagName("h4");
+var el = document.getElementsByClassName("name");
 for (var i = 0; i < el.length; i++){
     console.log(el);
     console.log(el[i]);
