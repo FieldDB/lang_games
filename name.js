9ce0c364d2df0e237315dcb59f7d3043d728a5ec
Carolyn Anderson <canders277@gmail.com>
2014-12-06 16:51:17 -0800

    js

--- name.js
+++ name.js
@@ -1,11 +1,29 @@
-function changeColor(item) {
-    console.log(item);
-  item.style = "color:red";    
+function changeColor(item, color) {
+    var tag = item.className;
+    var el = document.getElementsByClassName(tag);
+    var i;
+    console.log(el);
+    if (item.style.color === color) {
+        for (i = 0; i < el.length; i++){
+            console.log(el[i]);
+            el[i].style.color = "black";
+        }
+    } else {
+        for (var i = 0; i < el.length; i++){
+            console.log(el[i]);
+            el[i].style.color = "black";
+        }
+        item.style.color = color;
+    }
+    //this.addEventListener("click", function () {changeColor(this); }, false);
 }
 
-var el = document.getElementsByClassName("name");
-for (var i = 0; i < el.length; i++){
-    console.log(el);
-    console.log(el[i]);
-    el[i].addEventListener("click", changeColor(el[i]), false);
-}
+function addColorChange(tag, color) {
+    var el = document.getElementsByClassName(tag);
+    for (var i = 0; i < el.length; i++){
+         el[i].addEventListener("click", function () {changeColor(this, color); }, false);
+     }
+ }
+
+addColorChange("name", "red");
+addColorChange("color", "blue");
