77af81631cde9fc5e519aa375dbf615f4766e10d
Carolyn Anderson <canders277@gmail.com>
2014-12-16 09:52:41 -0800

    english data substituted

---
+++ _site/js/name_data_english.js
@@ -0,0 +1,28 @@
+/*English name data for name matching game, easy list*/
+var list = 
+    [
+    [
+        ["Mattie"], 
+        ["Matilda"]
+    ],
+    [
+        ["Marge"],
+        ["Margaret"]
+    ],
+    [
+        ["Harry"],
+        ["Harriet"]
+    ],
+    [
+        ["Carol"],
+        ["Carolyn"]
+    ],
+    [
+        ["Vic"],
+        ["Victor"]
+    ],
+    [
+        ["Dave"],
+        ["David"]
+    ]
+]
