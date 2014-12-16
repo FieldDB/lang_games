67652d7b72a1fe50b9439473eae17f989ad455ac
Carolyn Anderson <canders277@gmail.com>
2014-12-16 09:50:16 -0800

    english data

---
+++ _site/js/borrow_data_english.js
@@ -0,0 +1,31 @@
+/*English name data for borrowed words game.*/
+var data = [
+    [
+        [
+            ["a type of shoe"],
+            ["audio/moccasin.mp3"]
+        ],
+        ["img/moccasin.png"]
+    ],
+    [
+        [
+            ["Elk-like animal"],
+            ["audio/caribou.mp3"]
+        ],
+        ["img/caribou.png"]
+    ],
+    [
+        [
+            ["A kind of dwelling"],
+            ["audio/wigwam.mp3"]
+        ],
+        ["img/wigwam.png"]
+    ],
+    [
+        [
+            ["A type of sled"],
+            ["audio/toboggan.mp3"]
+        ],
+        ["img/toboggan.png"]
+    ]
+]
